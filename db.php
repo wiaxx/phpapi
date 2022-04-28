838cf36734d234097ecd5bf7fecd7ee305cd22f1
jaxhusen <jaxhusen@hotmail.com>
2022-04-28 16:51:26 +0200

    done

--- db.php
+++ db.php
@@ -4,7 +4,6 @@ $mysql_host = "localhost"; // våran host
 $mysql_user = "root";   // inlogg till mySQL
 $mysql_pass = "root"; // lösen till mySQL- kanske inte behövs ($mysql_pass = "root";)
 $mysql_db = "phpapi"; // namn på db i mySQL
-echo "bajs";
 
 $conn = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db); //connection mellan allt
 
